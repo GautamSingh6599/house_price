2ab24b40f68c37ef8be9c41515e7ed6ed7ed6494
Gautam Singh <gautam6599123@gmail.com>
2024-02-06 13:37:52 +0530

    Setup Env

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Determining Best Price for Houses on Sale',
+    author='Gautam Singh',
+    license='MIT',
+)
